2bb24760428922f958f12a175c14144f078dd9cb
Mansoor <98456377+amansoorsq@users.noreply.github.com>
2025-04-19 22:55:19 +0430

    updated homepage

--- gshare_project/core/views.py
+++ gshare_project/core/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
 def home(request):
     return render(request, 'home.html')
 
