8ede674433b8875f597ce467ed16687ba026d780
JD D <daviesj830@gmail.com>
2025-09-02 19:53:57 -0600

    fixed an issue with finding the url.

--- gshare_project/chat/routing.py
+++ gshare_project/chat/routing.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>[-\w]+)/$", consumers.ChatConsumer.as_asgi()),
     re_path(r"ws/chat/dm/(?P<thread_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
 
     # re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
