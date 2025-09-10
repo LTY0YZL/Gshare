c6740c03a3e954d64eaae46e6a38ab22ad70db0f
Mansoor <98456377+amansoorsq@users.noreply.github.com>
2025-09-10 10:48:18 -0600

    added button for order history

--- gshare_project/configurations/urls.py
+++ gshare_project/configurations/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('cart/add/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('shoppingcart/', views.shoppingcart, name='shoppingcart'),
-    path('myorders/', views.myorders, name='myorders'),
+    path('myorders/', views.myorders, name='order_history'),
     # path('chat/', include('chat.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
