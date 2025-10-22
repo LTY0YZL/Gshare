aadb5224677b067216747ddbb0a972a32238ea1c
LTY YZL <200384hy@gmail.com>
2025-10-22 16:40:58 -0600

    some merge bug fix

--- gshare_project/configurations/asgi.py
+++ gshare_project/configurations/asgi.py
@@ -6,15 +6,18 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configurations.settings')
+django.setup()
+
+from chat.routing import websocket_urlpatterns 
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
