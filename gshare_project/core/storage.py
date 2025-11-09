ce28271447079778452aa898492acfcfa76c0b2a
Anand Palukuri~ <u1223202@umail.utah.edu~>
2025-11-09 00:27:46 -0700

    updating settings the storage for images

--- gshare_project/core/storage.py
+++ gshare_project/core/storage.py
@@ -1,6 +1,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class PrivateMediaStorage(S3Boto3Storage):
+    bucket_name = "gshare-media-prod"
+    region_name = "us-east-2"
     default_acl = "private"
     file_overwrite = False
-    querystring_auth = True  # <- FORCE signed URLs
+    custom_domain = None        # force presigned URLs
+    querystring_auth = True     # generate signed links
