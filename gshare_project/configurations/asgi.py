99472784d05d78d38ec061a252066393a97c2f9a
Jason Davies <u1322589@umail.utah.edu>
2025-11-25 05:54:01 +0000

    Revert "updated the config files, so hopefully it all works now"
    This reverts commit c9ecbdb645f84570e9288856dcced38f9d511361

--- gshare_project/configurations/asgi.py
+++ gshare_project/configurations/asgi.py
@@ -1,18 +1,31 @@
+"""
+ASGI config for GShare project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
 import os
 import django
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configurations.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configurations.settings')
 django.setup()
 
+from chat.routing import websocket_urlpatterns 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
 })
+
+
+
+
